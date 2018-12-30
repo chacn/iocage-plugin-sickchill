72b1ea44dc333b329924a81439ca25460d96ff5f
chacn <chacn@users.noreply.github.com>
2018-12-30 10:08:56 -0600

    Change rall eferences to SickChill

--- post_install.sh
+++ post_install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # Create user and set up the directorys
-pw useradd -n sickrage -c "Sickrage" -s /sbin/nologin -w no
-fetch https://github.com/SickRage/SickRage/archive/master.tar.gz
+pw useradd -n sickchill -c "Sickchill" -s /sbin/nologin -w no
+fetch https://github.com/SickChill/SickChill/archive/master.tar.gz
 tar xfz master.tar.gz -C /usr/local/
-mv /usr/local/SickRage-master /usr/local/sickrage
-chown -R sickrage:sickrage /usr/local/sickrage
+mv /usr/local/SickChill-master /usr/local/sickchill
+chown -R sickchill:sickchill /usr/local/sickchill
 mkdir /usr/local/etc/rc.d
 
 # Enable
-sysrc -f /etc/rc.conf sickrage_enable="YES"
+sysrc -f /etc/rc.conf sickchill_enable="YES"
 
 # Copy the init script where it's useful
-cp /usr/local/sickrage/runscripts/init.freebsd /usr/local/etc/rc.d/sickrage
-chmod +x /usr/local/etc/rc.d/sickrage
+cp /usr/local/sickchill/runscripts/init.freebsd /usr/local/etc/rc.d/sickchill
+chmod +x /usr/local/etc/rc.d/sickchill
 
 # Start the service
-service sickrage start
+service sickchill start
